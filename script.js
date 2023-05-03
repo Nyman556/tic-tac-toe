d433cf666f3b86f99b76867f1db378654d561a12
Johan Nyman <johan.nyman97@gmail.com>
2023-05-03 13:04:50 +0200

    js update!

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
+const playerOne = [(id = 2), (sign = "o")];
+const playerTwo = [(id = 1), (sign = "x")];
+let turns = 0;
 cells = 9;
 board = document.getElementById("board");
+
+board.addEventListener("click", (event) => {
+	target = event.target;
+	if (target.classList.contains("played")) {
+		return;
+	}
+	if (turns % 2 === 0) {
+		target.classList.add(`played_by_player_${playerTwo[0]}`);
+		target.classList.add(`played`);
+		target.innerHTML = playerTwo[1];
+		return turns++;
+	} else {
+		target.classList.add(`played_by_player_${playerOne[0]}`);
+		target.classList.add(`played`);
+		target.innerHTML = playerOne[1];
+		return turns++;
+	}
+});
 
 function renderBoard() {
 	for (i = 0; i < cells; i++) {
